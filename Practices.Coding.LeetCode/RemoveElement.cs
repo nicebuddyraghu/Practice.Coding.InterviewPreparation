d9b8c60832bdf8ca05ed2a55e6419582a7b8a234
Raghu Sunkara <nicebuddyraghu@gmail.com>
2021-03-12 11:54:30 +0530

    Added RemoveElement Problem solved in LeetCode

---
+++ Practices.Coding.LeetCode/RemoveElement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices.Coding.LeetCode
+{
+    public class RemoveElement
+    {
+        public class Solution
+        {
+            public int RemoveElement(int[] nums, int val)
+            {
+
+                if (nums == null)
+                    return 0;
+
+                if (nums.Length > 100)
+                    return 0;
+
+                if (val > 100 || val < 0)
+                    return 0;
+
+                bool isValid = true;
+                var newnumlist = new System.Collections.Generic.List<int>();
+
+                foreach (var num in nums)
+                {
+                    if (num != val)
+                    {
+                        if (num > 50 || num < 0)
+                        {
+                            isValid = false;
+                            break;
+                        }
+
+                        newnumlist.Add(num);
+                    }
+                }
+
+                if (!isValid)
+                    return 0;
+
+                var index = 0;
+
+                foreach (var num in newnumlist)
+                {
+                    nums[index] = num;
+                    index++;
+                }
+
+                return newnumlist.Count;
+            }
+        }
+    }
+}
