083765ab46ec23279d3c0ba1abc010a54c125dd6
Raghu Sunkara <nicebuddyraghu@outlook.com>
2021-03-13 23:34:41 +0530

    Added Solution to the Problem
     https://leetcode.com/problems/climbing-stairs/submissions/

---
+++ Practices.Coding.LeetCode/ClimbStairs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices.Coding.LeetCode
+{
+    internal class ClimbStairs
+    {
+        //Dynamic Programming Example.
+        /***
+         * 
+         * 
+         * https://leetcode.com/problems/climbing-stairs/submissions/
+         * 
+         */
+
+        //Took 1 hour 10 minutes to solve this problem.
+        //1 Hour: 8 Minutes: 49 Seconds.
+
+        public class Solution
+        {
+
+            Dictionary<int, int> stepsClimbed = new Dictionary<int, int>();
+
+            public Solution()
+            {
+                stepsClimbed.Add(1, 1);
+                stepsClimbed.Add(2, 2);
+            }
+            public int ClimbStairs(int n)
+            {
+
+                if (n == 1)
+                {
+                    return stepsClimbed[1]; //return 1 step.
+                }
+                else if (n == 2)
+                {
+                    return stepsClimbed[2]; //return 2 step.
+                }
+                else
+                {
+                    if (!stepsClimbed.ContainsKey(n - 1))
+                        stepsClimbed.Add(n - 1, ClimbStairs(n - 1));
+
+                    if (!stepsClimbed.ContainsKey(n - 2))
+                        stepsClimbed.Add(n - 2, ClimbStairs(n - 2));
+
+                    return stepsClimbed[n - 1] + stepsClimbed[n - 2];
+                }
+            }
+        }
+    }
+}
--- Practices.Coding.LeetCode/Program.cs
+++ Practices.Coding.LeetCode/Program.cs
@@ -10,8 +10,10 @@ namespace Practices.Coding.LeetCode
             //Please execute or call the particular solution following the guidelines.
             //<ClassName>.<Solution>.<MethodName>
 
-          
-            
+            //ClimbStairs Example
+            ClimbStairs.Solution climbStairs = new ClimbStairs.Solution();
+            Console.WriteLine($"{climbStairs.ClimbStairs(7)}");
+            Console.ReadLine();
         }
     }
 }
