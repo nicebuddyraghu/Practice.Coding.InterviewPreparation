54b160f53ce2ffdd7f79616fbb98f4948c810953
Raghu Sunkara <nicebuddyraghu@gmail.com>
2021-04-04 18:47:05 +0530

    Added solution for https://leetcode.com/problems/water-bottles/submissions/

---
+++ Practices.Coding.LeetCode/WaterBottles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices.Coding.LeetCode
+{
+    internal class WaterBottles
+    {
+        /**
+         * 
+         * https://leetcode.com/problems/water-bottles/submissions/
+         * 
+         * 
+         * 
+         * 
+         */
+        internal class Solution
+        {
+            internal int NumWaterBottles(int numBottles, int numExchange)
+            {
+
+                int totalbottlescandrink = 0;
+
+                //Console.WriteLine(15%4);
+
+                int fullbottlesfromdrinkable = 0;
+
+                int tempBottles = numBottles;
+                while (tempBottles > 0 && tempBottles >= numExchange)
+                {
+                    fullbottlesfromdrinkable += tempBottles / numExchange;
+
+                    Console.WriteLine($"Drinkable bottles: {fullbottlesfromdrinkable}");
+                    tempBottles = (tempBottles / numExchange) + (tempBottles % numExchange);
+                    Console.WriteLine(tempBottles);
+                }
+
+                totalbottlescandrink = numBottles + fullbottlesfromdrinkable;
+
+                return totalbottlescandrink;
+            }
+        }
+    }
+}
