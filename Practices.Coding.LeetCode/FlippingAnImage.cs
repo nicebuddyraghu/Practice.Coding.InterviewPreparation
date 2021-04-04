aa90cf797c150945e0a369f155e8c3fa2ea494bc
Raghu Sunkara <nicebuddyraghu@gmail.com>
2021-04-04 19:24:10 +0530

    added solution for https://leetcode.com/problems/flipping-an-image/submissions/

---
+++ Practices.Coding.LeetCode/FlippingAnImage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices.Coding.LeetCode
+{
+    internal class FlippingAnImage
+    {
+        /***
+         * 
+         * 
+         * 
+         * https://leetcode.com/problems/flipping-an-image/submissions/
+         * 
+         * 
+         * 
+         */
+        internal class Solution
+        {
+            internal int[][] FlipAndInvertImage(int[][] image)
+            {
+
+                int[][] flippedimage = new int[image.Length][];
+
+                int flipfirstindex = 0;
+
+                for (int index = 0; index < image.Length; index++)
+                {
+                    int flipsecondindex = 0;
+                    flippedimage[flipfirstindex] = new int[image[index].Length];
+                    for (int jindex = image[index].Length - 1; jindex >= 0; jindex--)
+                    {
+                        flippedimage[flipfirstindex][flipsecondindex] = image[index][jindex];
+                        flipsecondindex++;
+                    }
+                    flipfirstindex++;
+                }
+
+                for (int index = 0; index < image.Length; index++)
+                {
+                    for (int jindex = image[index].Length - 1; jindex >= 0; jindex--)
+                    {
+                        var val = flippedimage[index][jindex];
+
+                        flippedimage[index][jindex] = val == 1 ? 0 : 1;
+                    }
+                }
+
+                return flippedimage;
+            }
+        }
+    }
+}
---
+++ Practices.Coding.LeetCode/SortedArrayByParity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices.Coding.LeetCode
+{
+    internal class SortedArrayByParity
+    {
+        /**
+         * 
+         *  https://leetcode.com/problems/sort-array-by-parity/submissions/
+         * 
+         * 
+         */
+        internal class Solution
+        {
+            internal int[] SortArrayByParity(int[] A)
+            {
+
+                int[] sorrtedarray = new int[A.Length];
+
+                int newindex = 0;
+                for (int index = 0; index < A.Length; index++)
+                {
+                    if (A[index] % 2 == 0)
+                    {
+                        sorrtedarray[newindex] = A[index];
+                        newindex++;
+                    }
+                }
+
+                for (int index = 0; index < A.Length; index++)
+                {
+                    if (A[index] % 2 != 0)
+                    {
+                        sorrtedarray[newindex] = A[index];
+                        newindex++;
+                    }
+                }
+
+                return sorrtedarray;
+            }
+        }
+    }
+}
