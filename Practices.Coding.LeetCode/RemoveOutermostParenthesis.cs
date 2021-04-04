598245cc301ccd3052513f3b20c64647a6536d5e
Raghu Sunkara <nicebuddyraghu@gmail.com>
2021-04-04 13:27:23 +0530

    added solutions for https://leetcode.com/problems/remove-outermost-parentheses/submissions/

---
+++ Practices.Coding.LeetCode/RemoveOutermostParenthesis.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices.Coding.LeetCode
+{
+    internal class RemoveOutermostParenthesis
+    {
+        /**
+         * 
+         * https://leetcode.com/problems/remove-outermost-parentheses/submissions/
+         * 
+         * 
+         * 
+         */
+        internal class Solution
+        {
+            internal string RemoveOuterParentheses(string S)
+            {
+
+                int count = 0;
+                Queue<char> charstack = new Queue<char>();
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var ch in S)
+                {
+                    if (ch == '(')
+                    {
+                        count++;
+                        Console.WriteLine(count);
+                        charstack.Enqueue('(');
+
+                    }
+                    else
+                    {
+                        count--;
+                        Console.WriteLine(count);
+                        charstack.Enqueue(')');
+
+                        if (count == 0)
+                        {
+                            if (charstack.Count > 1)
+                                charstack.Dequeue();
+
+                            while (charstack.Count > 1)
+                            {
+                                sb.Append(charstack.Dequeue());
+                            }
+
+                            if (charstack.Count == 1)
+                                charstack.Dequeue();
+                        }
+
+
+                    }
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
