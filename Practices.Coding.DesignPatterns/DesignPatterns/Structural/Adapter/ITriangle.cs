07c353477a4f0dd60a34d881965c0d20238e8d42
Raghu Sunkara <nicebuddyraghu@gmail.com>
2022-05-16 17:40:30 +0530

    Added few design patterns

---
+++ Practices.Coding.DesignPatterns/DesignPatterns/Structural/Adapter/ITriangle.cs
@@ -0,0 +1,8 @@
+﻿namespace Practices.Coding.DesignPatterns.DesignPatterns.Structural.Adapter
+{
+    internal interface ITriangle
+    {
+        void AboutTriangle();
+        double CalculatreAreaofTriangle();
+    }
+}
---
+++ Practices.Coding.DesignPatterns/DesignPatterns/Structural/Adapter/Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practices.Coding.DesignPatterns.DesignPatterns.Structural.Adapter
+{
+    class Triangle : ITriangle
+    {
+        private double baseLength;
+        private double height;
+        public Triangle(double baselength, double height)
+        {
+            this.baseLength = baselength;
+            this.height = height;
+        }
+        public void AboutTriangle()
+        {
+            Console.WriteLine("Acutally, I am a Triangle");
+        }
+
+        public double CalculatreAreaofTriangle()
+        {
+            return 0.5 * baseLength * height;
+        }
+    }
+}
