a3cfd0250ace13c99c6f35573bcf567763e9d4c3
Raghu Sunkara <nicebuddyraghu@gmail.com>
2022-05-16 17:56:04 +0530

    Added few additional design patterns

---
+++ Practices.Coding.DesignPatterns/DesignPatterns/Structural/Facade/RobotColor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Practices.Coding.DesignPatterns.DesignPatterns.Structural.Facade
+{
+    public class RobotColor
+    {
+        public void SetDefaultColor() { Console.WriteLine("This is steel color robot."); }
+        public void SetGreenColor() { Console.WriteLine("This is a green color robot."); }
+    }
+}
