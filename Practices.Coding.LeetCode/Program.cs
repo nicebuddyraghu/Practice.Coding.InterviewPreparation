5e1033ead97a480adf38b12ca74d2d7b7b97c7b1
Raghu Sunkara <nicebuddyraghu@gmail.com>
2022-05-17 23:20:15 +0530

    Added Few Leetcode problem solving.

--- Practices.Coding.LeetCode/Program.cs
+++ Practices.Coding.LeetCode/Program.cs
@@ -1,5 +1,5 @@
-﻿using Practices.Coding.LeetCode.Sorting;
-using System;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Practices.Coding.LeetCode
 {
@@ -12,32 +12,34 @@ namespace Practices.Coding.LeetCode
             //<ClassName>.<Solution>.<MethodName>
 
             //ClimbStairs Example
-            var heap = new Heap.Heap(10, Heap.HeapType.Min);
-            heap.BuildHeap(heap, new int[] { 10, 4, 43, 5, 57, 91, 45, 9, 7, 1 }, 10);
+            //var heap = new Heap.Heap(10, Heap.HeapType.Min);
+            //heap.BuildHeap(heap, new int[] { 10, 4, 43, 5, 57, 91, 45, 9, 7, 1 }, 10);
 
-            foreach (var element in heap.elements)
-            {
-                Console.Write($"->{element}");
-            }
+            //foreach (var element in heap.elements)
+            //{
+            //    Console.Write($"->{element}");
+            //}
 
-            heap = new Heap.Heap(10, Heap.HeapType.Min);
-            heap.Insert(heap, 10);
-            heap.Insert(heap, 4);
-            heap.Insert(heap, 43);
-            heap.Insert(heap, 5);
-            heap.Insert(heap, 57);
-            heap.Insert(heap, 91);
-            heap.Insert(heap, 45); 
-            heap.Insert(heap, 9);
-            heap.Insert(heap, 7);
-            heap.Insert(heap, 1);
-            
-            Console.WriteLine();
-            foreach (var element in heap.elements)
-            {
-                Console.Write($"->{element}");
-            }
+            //heap = new Heap.Heap(10, Heap.HeapType.Min);
+            //heap.Insert(heap, 10);
+            //heap.Insert(heap, 4);
+            //heap.Insert(heap, 43);
+            //heap.Insert(heap, 5);
+            //heap.Insert(heap, 57);
+            //heap.Insert(heap, 91);
+            //heap.Insert(heap, 45); 
+            //heap.Insert(heap, 9);
+            //heap.Insert(heap, 7);
+            //heap.Insert(heap, 1);
 
+            //Console.WriteLine();
+            //foreach (var element in heap.elements)
+            //{
+            //    Console.Write($"->{element}");
+            //}
+
+            //BraceExpansion braceExpansion = new BraceExpansion();
+            //var list = braceExpansion.Solution("{a,b,c}de{f,g,h}i{j,k}");
 
             Console.WriteLine();
             Console.WriteLine("Press Any Key To Continue!!");
