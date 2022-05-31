e0a72691e9ebf530a04cec2878b4259585ebbb2f
Raghu Sunkara <nicebuddyraghu@gmail.com>
2022-06-01 03:02:15 +0530

    added few leetcode solutions.

---
+++ Practices.Coding.LeetCode/Difficulty.Medium/PermutationsIntegers.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices.Coding.LeetCode.Difficulty.Medium
+{
+    /***
+     * 
+     * 
+     * https://leetcode.com/problems/permutations/
+     * 
+     * 
+     */
+    public class PermutationsIntegers
+    {
+        public class Solution
+        {
+
+            public IList<IList<int>> Permute(int[] nums)
+            {
+                var listofList = Permute(nums, 0, nums.Length - 1);
+                IList<int> newlist = new List<int>();
+                foreach (var element in nums)
+                {
+                    newlist.Add(element);
+                }
+
+                listofList.Add(newlist);
+
+                return listofList;
+            }
+
+            public IList<IList<int>> Permute(int[] nums, int start, int end)
+            {
+                IList<IList<int>> listOfList = new List<IList<int>>();
+
+
+                if (start < end)
+                {
+                    for (int index = start; index <= end; index++)
+                    {
+                        var temp = nums[end];
+                        for (int moveindex = end; moveindex >= start + 1; moveindex--)
+                        {
+                            nums[moveindex] = nums[moveindex - 1];
+                        }
+                        nums[start] = temp;
+
+                        if (index != end)
+                        {
+                            IList<int> newlist = new List<int>();
+                            foreach (var element in nums)
+                            {
+                                newlist.Add(element);
+                            }
+
+
+                            listOfList.Add(newlist);
+                        }
+
+                        var sublistofList = Permute(nums, start + 1, end);
+
+                        foreach (var list in sublistofList)
+                        {
+                            listOfList.Add(list);
+                        }
+
+                    }
+                }
+                return listOfList;
+            }
+        }
+    }
+}
---
+++ Practices.Coding.LeetCode/Difficulty.Medium/TrieImplementation.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices.Coding.LeetCode.Difficulty.Medium
+{
+    /*
+     * 
+     * https://leetcode.com/problems/implement-trie-prefix-tree/
+     * 
+     * 
+     */
+    public class TrieImplementation
+    {
+        public class TrieNode
+        {
+            public static int AlphabetSize = 26;
+
+            public TrieNode[] ChildNodes { get; }
+            public char Data { get; }
+            public int EndOfWord { get; set; }
+
+            public TrieNode(char data)
+            {
+                Data = data;
+                EndOfWord = 0;
+                ChildNodes = new TrieNode[TrieNode.AlphabetSize];
+                for (int index = 0; index < 26; index++)
+                {
+                    ChildNodes[index] = null;
+                }
+            }
+        }
+        public class Trie
+        {
+
+            TrieNode root = new TrieNode('/');
+
+            public Trie()
+            {
+
+            }
+
+            public void Insert(string word)
+            {
+
+                var rootnode = root;
+
+                for (int index = 0; index <= word.Length - 1; index++)
+                {
+                    int trieindex = word[index] - 'a';
+                    TrieNode childNode = null;
+                    if (rootnode.ChildNodes[trieindex] == null)
+                    {
+                        childNode = new TrieNode(word[index]);
+                        rootnode.ChildNodes[trieindex] = childNode;
+                    }
+                    else
+                    {
+                        childNode = rootnode.ChildNodes[trieindex];
+                    }
+
+                    if (index == word.Length - 1)
+                    {
+                        childNode.EndOfWord = 1;
+                    }
+                    rootnode = childNode;
+                }
+            }
+
+            public bool Search(string word)
+            {
+
+                var rootnode = root;
+
+                for (int index = 0; index <= word.Length - 1; index++)
+                {
+                    int trieindex = word[index] - 'a';
+                    TrieNode childNode = null;
+                    if (rootnode.ChildNodes[trieindex] == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        childNode = rootnode.ChildNodes[trieindex];
+                    }
+
+                    if (childNode == null)
+                        return false;
+                    if (index == word.Length - 1 && childNode.EndOfWord == 1)
+                    {
+                        return true;
+                    }
+                    rootnode = childNode;
+                }
+
+                return false;
+            }
+
+            public bool StartsWith(string prefix)
+            {
+                var rootnode = root;
+
+                for (int index = 0; index <= prefix.Length - 1; index++)
+                {
+                    int trieindex = prefix[index] - 'a';
+                    TrieNode childNode = null;
+                    if (rootnode.ChildNodes[trieindex] == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        childNode = rootnode.ChildNodes[trieindex];
+                    }
+
+                    if (childNode == null)
+                        return false;
+
+                    if (index == prefix.Length - 1)
+                    {
+                        return true;
+                    }
+                    rootnode = childNode;
+                }
+
+                return false;
+            }
+        }
+
+        /**
+         * Your Trie object will be instantiated and called as such:
+         * Trie obj = new Trie();
+         * obj.Insert(word);
+         * bool param_2 = obj.Search(word);
+         * bool param_3 = obj.StartsWith(prefix);
+         */
+    }
+}
